08fd81bc36be0ed3caa599dd8a15bc3fd4b95d94
Christophe Opoix <christphe@users.noreply.github.com>
2020-09-16 03:52:56 +0200

    changed play_sound signature

--- src/actors/Actor.gd
+++ src/actors/Actor.gd
@@ -4,7 +4,7 @@ class_name Actor
 func _process(delta: float) -> void:
 	z_index = position.y
 
-func play_sound(sound_player_name: String, sound: AudioStream) -> void:
+func play_sound(sound_player_name: NodePath, sound: AudioStream) -> void:
 	var player = get_node(sound_player_name) as AudioStreamPlayer
 	player.stream = sound
 	player.play()
